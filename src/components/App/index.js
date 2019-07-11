a51fa7091403a39d87e5a031625132df1a318534
omar <omasrt@gmail.com>
2019-07-11 12:07:03 -0500

    first commit

---
+++ src/components/App/index.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
+import Navigation from "../Navigation";
+import LandingPage from "../Landing";
+import HomePage from "../Home";
+
+import * as ROUTES from "../../constants/routes";
+
+const App = () => (
+  <Router>
+    <div>
+      <Navigation />
+      <hr />
+
+      <Route exact path={ROUTES.LANDING} component={LandingPage} />
+      <Route path={ROUTES.HOME} component={HomePage} />
+    </div>
+  </Router>
+);
+
+export default App;
