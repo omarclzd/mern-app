5c9912dd2f1e30c97e12a859d32b111f85aabd85
omar <omasrt@gmail.com>
2019-07-12 12:21:02 -0500

    sets handleClick

--- src/components/Landing/index.js
+++ src/components/Landing/index.js
@@ -1,29 +1,64 @@
 import React, { Component } from "react";
+import "./Landing.css";
+import Card from "@material-ui/core/Card";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
+import Button from "@material-ui/core/Button";
+
+import CssBaseline from "@material-ui/core/CssBaseline";
+
+import Container from "@material-ui/core/Container";
+
 import { getAdvice } from "../../services/ad-api";
 
 class Landing extends Component {
   state = {
-    advices: []
+    advices: [],
+    adviceId: null
   };
 
-  getAdvice = id => {
-    return this.state.advices[id];
+  getAdvice = () => {
+    return this.state.advices;
+  };
+
+  handleClick = () => {
+    this.componentDidMount();
   };
 
   async componentDidMount() {
     const adviceData = await getAdvice();
     this.setState({
-      advices: adviceData.slip.advice
+      advices: adviceData
+      // adviceId: adviceData.slip.slip_id
     });
-    console.log(adviceData.slip.advice);
+    console.log(this.state);
   }
 
   render() {
     return (
-      <div>
-        <header>Advices</header>
-        <div>{this.state.advices}</div>
-      </div>
+      <React.Fragment>
+        <CssBaseline />
+        <Container fixed>
+          <h2>Advices</h2>
+          <Card>
+            <CardContent>
+              <div>Advice of the day</div>
+              <div className="advice">{this.state.advices}</div>
+            </CardContent>
+            <CardActions>
+              <Button size="small">Learn More</Button>
+            </CardActions>
+          </Card>
+          <Button
+            onClick={this.handleClick}
+            size="small"
+            variant="contained"
+            color="primary"
+          >
+            Get new advice
+          </Button>
+        </Container>
+      </React.Fragment>
     );
   }
 }
--- src/services/ad-api.js
+++ src/services/ad-api.js
@@ -1,4 +1,5 @@
 export function getAdvice() {
-  const endPoint = "https://api.adviceslip.com/advice";
+  // const endPoint = "https://api.adviceslip.com/advice";
+  const endPoint = "https://ron-swanson-quotes.herokuapp.com/v2/quotes";
   return fetch(endPoint, { mode: "cors" }).then(res => res.json());
 }
