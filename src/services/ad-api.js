0be8e0bfc78672188ec4c6135536459d09a8079d
omar <omasrt@gmail.com>
2019-07-13 23:17:53 -0500

    sets new api in services

--- src/services/ad-api.js
+++ src/services/ad-api.js
@@ -4,3 +4,10 @@ export function getAdvice() {
   const endPoint = "http://ergast.com/api/f1/2019/last/results.json";
   return fetch(endPoint, { mode: "cors" }).then(res => res.json());
 }
+
+// export function getTeam() {
+//   // const endPoint = "https://api.adviceslip.com/advice";
+//   // const endPoint = "https://ron-swanson-quotes.herokuapp.com/v2/quotes";
+//   const endPoint = "http://ergast.com/api/f1/2019/last/results.json";
+//   return fetch(endPoint, { mode: "cors" }).then(res => res.json());
+// }
