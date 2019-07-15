cee705b79b0f721104946efe83f0be2e896b1f4f
omar <omasrt@gmail.com>
2019-07-15 13:43:42 -0500

    adds img to driver info

--- src/components/DriverPage/DriverPage.js
+++ src/components/DriverPage/DriverPage.js
@@ -1,46 +1,56 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "./DriverPage.css";
 import * as ROUTES from "../../constants/routes";
-
-import CssBaseline from "@material-ui/core/CssBaseline";
-import Container from "@material-ui/core/Container";
 
 const DriverPage = props => {
   const driver = props.getAdvice(props.match.params.idx);
   let drp = props.user ? (
-    <div>
-      <React.Fragment>
-        <CssBaseline />
-        <Container fixed>
-          <div>
-            {driver ? (
-              <div>
-                <div>
-                  <h2>Driver Info</h2>
-                  <div>
-                    Name: {driver.Driver.givenName} {driver.Driver.familyName}
+    <div className="stats">
+      <div className="container">
+        <div className="stats">
+          {driver ? (
+            <div className="stats">
+              <div className="card mb-3">
+                <div className="row no-gutters">
+                  <div className="col-md-4">
+                    <img
+                      src="https://i.imgur.com/lS95joQ.jpg"
+                      className="card-img"
+                      alt=""
+                    />
                   </div>
-                  <div>Number: {driver.number}</div>
-                  <div>DOB: {driver.Driver.dateOfBirth}</div>
-                  <div>Nationality: {driver.Driver.nationality}</div>
-                  <div>Team: {driver.Constructor.name}</div>
+                  <div className="col-md-8">
+                    <h2 className="card-title text-center">Driver Info</h2>
+                    <div className="card-body">
+                      <p>
+                        Name: {driver.Driver.givenName}{" "}
+                        {driver.Driver.familyName}
+                      </p>
+                      <p>Number: {driver.number}</p>
+                      <p>DOB: {driver.Driver.dateOfBirth}</p>
+                      <p>Nationality: {driver.Driver.nationality}</p>
+                      <p>Team: {driver.Constructor.name}</p>
+                    </div>
+                  </div>
                 </div>
-                <h2>Driver's Race Stats</h2>
-                <div>Starting Position: {driver.grid}</div>
-                <div>Ending Position: {driver.position}</div>
-                <div>Total Laps: {driver.laps}</div>
-                <div>Race Status: {driver.status}</div>
-                <div>Fastest Lap: Lap #{driver.FastestLap.lap}</div>
-                <div>Fastest Lap Time: {driver.FastestLap.Time.time}</div>
-                <div />
-                <Link to={ROUTES.LANDING}>Return</Link>
               </div>
-            ) : (
-              <h3>Loading...</h3>
-            )}
-          </div>
-        </Container>
-      </React.Fragment>
+
+              <h2>Driver's Race Stats</h2>
+              <p>Starting Position: {driver.grid}</p>
+              <p>Ending Position: {driver.position}</p>
+              <p>Total Laps: {driver.laps}</p>
+              <p>Race Status: {driver.status}</p>
+              <p>Fastest Lap: Lap #{driver.FastestLap.lap}</p>
+              <p>Fastest Lap Time: {driver.FastestLap.Time.time}</p>
+              <div />
+              <Link to={ROUTES.LANDING}>Return</Link>
+            </div>
+          ) : (
+            <h3>Loading...</h3>
+          )}
+        </div>
+      </div>
     </div>
   ) : (
     <div>
